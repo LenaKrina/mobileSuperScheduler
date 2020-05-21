6b4f1667ef2c49a2d350cb8a25104b5b7baacda5
LenaKrina <54749924+LenaKrina@users.noreply.github.com>
2020-05-21 15:11:30 +0300

    created project SuperScheduler

---
+++ src/test/java/com/superscheduler/mobile/manager/ApplicationManager.java
@@ -0,0 +1,68 @@
+package com.superscheduler.mobile.manager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+    WebDriver wd;
+    // BoardHelper board;
+    // TeamHelper team;
+    SessionHelper session;
+    String browser;
+    // ProfileHelper profile;
+    Properties properties;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+
+
+    public void init() throws InterruptedException, IOException {
+        if(browser.equals(org.openqa.selenium.remote.BrowserType.CHROME)){
+            wd = new ChromeDriver();
+        } if(browser.equals(BrowserType.FIREFOX)){
+            wd = new FirefoxDriver();
+        }
+
+
+        wd.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        wd.manage().window().maximize();
+
+        properties = new Properties();
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+        wd.navigate().to(properties.getProperty("web.baseURL"));
+
+        session = new SessionHelper(wd);
+        session.login(properties.getProperty("web.user"), properties.getProperty("web.password"));
+        Thread.sleep(3000);
+
+
+    }
+
+    public void stop() {
+        wd.quit();
+    }
+
+    public boolean isOnBoardsPage() throws InterruptedException {
+        Thread.sleep(4000);
+        String url = wd.getCurrentUrl();
+        return url.contains("boards");
+    }
+
+
+    public SessionHelper getSession() {
+        return session;
+    }
+
+}
---
+++ src/test/java/com/superscheduler/mobile/manager/SessionHelper.java
@@ -0,0 +1,31 @@
+package com.superscheduler.mobile.manager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SessionHelper extends HelperBase {
+
+    public SessionHelper(WebDriver wd) {
+        super(wd);
+    }
+
+    public void login(String email, String password) {
+        initLogin();
+        fillLoginForm(email, password);
+        confirmLogin();
+    }
+
+    public void confirmLogin() {
+        waitForElementLocatedAndclick(By.cssSelector("#login"), 20);
+    }
+
+    public void fillLoginForm(String userEmail, String password) {
+        waitForElementLocatedAndclick(By.name("user"), 20);
+        type(By.cssSelector("#user"), userEmail);
+        type(By.cssSelector("#password"), password);
+    }
+
+    public void initLogin() {
+        waitForElementLocatedAndclick(By.cssSelector("[href='/login']"), 20);
+    }
+}
